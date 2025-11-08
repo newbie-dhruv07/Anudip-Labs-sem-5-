ca9f81fc06d9246f9542698fac56e105b3a01532
newbie-dhruv07 <jaindhruv2005@gmail.com>
2025-11-08 22:13:26 +0530

    Add files via upload
    lab-1, assignment-2

---
+++ Employee.java
@@ -0,0 +1,11 @@
+public class Employee{
+protected int id;
+protected int age =(int)35.5;
+protected String name;
+protected boolean isPermanent;
+public static void main (String []args)
+{
+  System.out.println("Successfully started");
+}
+
+}
